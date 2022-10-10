8e09fe32056e0c66edf512d4099965aca9369a9e
hmathir <hmathir@gmail.com>
2022-10-11 02:46:42 +0600

    Statistics Added

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -14,15 +14,36 @@ const Statistics = () => {
   const data = useContext(QuizContex);
 
   return (
-    <BarChart width={730} height={250} data={data}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis dataKey />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="pv" fill="#8884d8" />
-      <Bar dataKey="uv" fill="#82ca9d" />
-    </BarChart>
+    <div className="w-9/12 mx-auto mt-10">
+      <div className="text-3xl">
+        <h1>Chart for Quiz Amount and ID:</h1>
+      </div>
+      <div className="md:flex gap-5 justify-between">
+        <div className="mt-20">
+          <BarChart width={330} height={450} data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis dataKey="total" />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="total" fill="#8823d8" />
+          </BarChart>
+        </div>
+        <div className="mt-20">
+          <BarChart width={330} height={450} data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis dataKey="id" />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="id" fill="#8884d8" />
+          </BarChart>
+        </div>
+      </div>
+
+
+    </div>
+
   );
 };
 
